0f34f7623dccde4711ff04ac0a120c6663a23c16
EmilyJYN <yiningjin111@gmail.com>
2023-06-05 14:57:34 -0400

    add comment to outputStreams.cpp

--- lectures/L4-streams/outputStreams.cpp
+++ lectures/L4-streams/outputStreams.cpp
@@ -11,3 +11,8 @@ int main() {
    outputFile << "My favorite number is: " << bestNumber << endl;
    return 0;
 }
+/*这段代码在第二次运行时，会将新的内容覆盖写入 out.txt 文件，而不是追加到文件的末尾。
+这是因为在创建 std::ofstream 对象时，默认情况下文件模式是截断模式（std::ios::trunc），
+这意味着在打开文件时会清空文件内容，然后写入新的内容。
+如果您希望将新的内容追加到文件的末尾，而不是覆盖原有内容，可以在打开文件时指定追加模式（std::ios::app）
+*/
