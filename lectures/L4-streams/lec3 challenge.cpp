725535fca6e1c0a0a5d24386097aab72a4dabb2d
EmilyJYN <yiningjin111@gmail.com>
2023-06-04 23:53:57 -0400

    minor bug fix for lec3 challenge.cpp

--- lectures/L4-streams/lec3 challenge.cpp
+++ lectures/L4-streams/lec3 challenge.cpp
@@ -7,7 +7,7 @@ using std::cout;  using std::cin;  using std::endl; using std::cerr;
 
 string prompUserFile(ifstream& stream, 
                      string prompt ="Please enter a file name: ", 
-                     string reprompt = "Invalid file name, please re-enter a file name: ");
+                     string reprompt = "Invalid file name, please re-enter a file name. ");
 
 /* Write the following function which 
 prompts the user for a filename, opens the ifstream to the file,
@@ -25,6 +25,7 @@ string prompUserFile(ifstream& stream, string prompt, string reprompt){
             stream.open(filename); //open file
             if (stream) return filename; // if stream's fail bit is not on
         }
+        cerr << reprompt << endl;
     }
     
 }
