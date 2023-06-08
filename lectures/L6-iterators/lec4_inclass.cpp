aec453645d38cc4940bbf684c4f8fd1276186687
EmilyJYN <52183621+EmilyJYN@users.noreply.github.com>
2023-06-08 18:52:26 -0400

    lec 4 inclass code for iterator

---
+++ lectures/L6-iterators/lec4_inclass.cpp
@@ -0,0 +1,28 @@
+#include <iterator>
+#include <set>
+#include <iostream>
+
+using std::cout; using std::endl;
+using std::set;
+
+int main(){
+
+    set<int> container;
+    for (int i = 0; i < 10; i++){
+        container.insert(i);
+    }
+
+    //set<int>::iterator itr = container.begin()
+    auto itr = container.begin();
+    while (itr != container.end()){
+        cout << *itr << " ";
+        ++itr;
+    }
+    cout << endl;
+
+    set<int>::iterator itr2;
+    for(itr2 = container.begin(); itr2 != container.end(); ++itr2){
+        cout << *itr2 << " ";
+    }
+    cout << endl;
+}
