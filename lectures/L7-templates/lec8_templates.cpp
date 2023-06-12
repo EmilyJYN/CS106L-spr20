83a7d5e89cf65e81c676b0aa13c1081d54d5f41b
EmilyJYN <52183621+EmilyJYN@users.noreply.github.com>
2023-06-11 23:37:38 -0400

    update predicate funtion to a  lambda dunction

--- lectures/L7-templates/lec8_templates.cpp
+++ lectures/L7-templates/lec8_templates.cpp
@@ -20,11 +20,11 @@ bool isDivisibleBy(int dividend, int divisor){
 
 
 template <typename InputIterator, typename UnaryPredicate> //定义了两个template parameter: InputIterator, UnaryPredicate
-int countOccurences(InputIterator begin, InputIterator end, UnaryPredicate predicate){
+int countOccurences(InputIterator begin, InputIterator end, UnaryPredicate predicate){ //这里pass的function是这个function的pointer
 
     int count = 0;
     for(auto iter = begin; iter != end; ++iter){
-        if(predicate(*iter)) count++;
+        if(predicate(*iter)) count++; //pass one of the element of the container into the predicate function
     }
     return count;
 }
@@ -35,6 +35,19 @@ bool isLessThan5(int val){
 
 int main(){
     vector<int> vec{1,3,5,7,9};
-    cout << countOccurences(vec.begin(), vec.end(), isLessThan5) << endl; //isLessThan5不加()，否则报错
+    
+    int limit = 5;
+    //atuo val, 这里起的作用类似于一个template，使这个lambda函数可以输入很多种type的变量
+    auto isLessThanLimit = [limit](auto val) -> bool{ 
+        return val < limit;
+    };
+
+    //cout << "Occurence of # less than 5: ";
+    //cout << countOccurences(vec.begin(), vec.end(), isLessThan5) << endl; //isLessThan5不加()，否则报错
+    cout << "Occurence of # less than 5: ";
+    cout << countOccurences(vec.begin(), vec.end(), isLessThanLimit) << endl; //isLessThan5不加()，否则报错
+    cout << "Occurence of # less than 5 in second half: ";
+    cout << countOccurences(vec.begin()+vec.size()/2, vec.end(), isLessThanLimit) << endl; 
+
     return 0;
 }
